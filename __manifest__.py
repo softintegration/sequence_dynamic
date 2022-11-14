657f28232ce762356a982f6a4fba546119ef8608
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-11-14 14:58:35 +0100

    [IMP] Update form view field positions & Improve intuitiveness of interface.

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Dynamic sequence',
-    'version': '1.0.1.4',
+    'version': '1.0.1.5',
     'author':'Soft-integration',
     'category': 'Base',
     'summary': 'Dynamic sequence',
