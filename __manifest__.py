bbc659967012700e0f1d36f2e9252224957ffd90
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-19 12:06:54 +0100

    [DOC] Sequence dynamice:Add the possibility to force name

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Dynamic sequence',
-    'version': '1.0.4.7',
+    'version': '1.0.4.8',
     'author':'Soft-integration',
     'category': 'Base',
     'summary': 'Dynamic sequence',
